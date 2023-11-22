d30d577ca72a6d444369408d4ec8d0051beaeded
AhmedHHamdy <ahmedhhamdyad@gmail.com>
2023-11-22 20:30:05 +0200

    Fix a Styling bug

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -26,30 +26,36 @@ export default function Header() {
 
   useEffect(() => {
     document.documentElement.dir = i18n.dir();
-    // Create a style element outside the condition
-    const styleElement = document.createElement('style');
+  // Remove existing style elements
+  document.querySelectorAll('style[data-dynamic-style]').forEach((element) => {
+    element.parentNode.removeChild(element);
+  });
 
-    if (i18n.language === 'ar') {
-      import('../App.rtl.css').then((module) => {
-        // Apply styles dynamically
-        styleElement.textContent = module.default;
-        document.head.appendChild(styleElement);
-      });
-    } else if (i18n.language === 'en') {
-      import('../App.css').then((module) => {
-      //   // Apply styles dynamically
-        styleElement.textContent = module.default;
-        document.head.appendChild(styleElement);
-      });
+  // Create a new style element
+  const styleElement = document.createElement('style');
+  styleElement.setAttribute('data-dynamic-style', 'true'); // Set a data attribute to identify dynamic styles
+
+  if (i18n.language === 'ar') {
+    import('../App.rtl.css').then((module) => {
+      // Apply styles dynamically
+      styleElement.textContent = module.default;
+      document.head.appendChild(styleElement);
+    });
+  } else if (i18n.language === 'en') {
+    import('../App.css').then((module) => {
+      // Apply styles dynamically
+      styleElement.textContent = module.default;
+      document.head.appendChild(styleElement);
+    });
+  }
+
+  // Remove the current style element when the component is unmounted or when the language changes
+  return () => {
+    if (styleElement.parentNode) {
+      styleElement.parentNode.removeChild(styleElement);
     }
-
-    // Remove old styles (this will remove the previous styles on each language change)
-    return () => {
-      if (styleElement.parentNode) {
-        document.head.removeChild(styleElement);
-      }
-    };
-  }, [i18n.language]);
+  };
+}, [i18n.language]);
 
 
   const { token, setToken } = useAuth()
